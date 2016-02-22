10c8ba33811f161271835bde78f808537bfedd34
Pratik Anand <pratik.preet@gmail.com>
2016-02-22 18:40:33 +0530

    working video player basic

--- window.py
+++ window.py
@@ -1,15 +1,25 @@
 import sys
+import os
 import basic_ui
 import signals_slots
 from PyQt4 import QtGui, QtCore
+from PyQt4.phonon import Phonon 
 
 class Window(QtGui.QMainWindow):
 
+
     def __init__(self):
         super(Window, self).__init__()
+        self.signals_slots = signals_slots.Signal_Slots()
     
     def setup_connections( self, ui ) :
         ui.actionExit.triggered.connect(QtCore.QCoreApplication.instance().quit )
+        ui.actionOpen.triggered.connect(lambda: self.signals_slots.open_file(self, ui))
+        ui.playButton.clicked.connect(lambda: self.signals_slots.start_playback(self, ui))
+        ui.stopButton.clicked.connect(lambda: self.signals_slots.stop_playback(self, ui))
+
+
+
 
 def run():
     app = QtGui.QApplication(sys.argv)
