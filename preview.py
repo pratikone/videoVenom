956d0b9fd27d48aafcd18881ee00a68c3df8813b
Pratik Anand <pratik.preet@gmail.com>
2016-03-06 23:26:14 +0530

    full image creation working

--- preview.py
+++ preview.py
@@ -13,12 +13,16 @@ class Preview(QtGui.QWidget) :
     def __init__(self, ui):
         super(Preview, self).__init__()
         self.ui = ui
+        self.font = self.color = self.image = None
         
 
     def setup_connections(self) :
         self.ui.pushButton.clicked.connect( self.closeWidget )
 
     def moveStuff(self, scaleFactor, bannerCoords, bannerLabelCoords, bannerLabelText, font, color, image) :
+
+        
+
         self.resize( scaleFactor * self.geometry().width(), scaleFactor * self.geometry().height() )
         self.ui.bgLabel.resize( scaleFactor * self.ui.bgLabel.geometry().width(), scaleFactor * self.ui.bgLabel.geometry().height() )
         
@@ -30,21 +34,47 @@ class Preview(QtGui.QWidget) :
 
         self.ui.bannerLabel.setText( bannerLabelText )
         if font is not None :
+            self.font = font
             self.ui.bannerLabel.setFont(font)
         if color is not None :
+            self.color = color
             self.ui.bannerLabel.setStyleSheet("QLabel { color: %s}" % color.name())
         
         if image is not None :
             pixmap = QtGui.QPixmap( image )
             self.ui.bannerlImage.setPixmap( pixmap.scaled( self.ui.bannerlImage.width(), self.ui.bannerlImage.height()))
+            self.image = image
         
         self.ui.bannerLabel.raise_() #raise banner text on top of banner image
 
         self.repaint()
 
+
+    def saveImage(self) :
+        image =  QtGui.QImage(self.geometry().width(), self.geometry().height()  , QtGui.QImage.Format_ARGB32_Premultiplied);
+        qp = QtGui.QPainter()
+        qp.begin(image)
+        # qp.fillRect(self.ui.bannerlImage.geometry(), Qt::yellow);
+        # pix = QtGui.QPixmap(image)
+        # qp.drawPixmap( QtCore.QRect(50, 50, 100, 100), pix, QtCore.QRect(50, 50, 100, 100) )
+        if self.image is not None :
+            qp.drawPixmap( self.ui.bannerlImage.geometry(), self.ui.bannerlImage.pixmap(),  self.ui.bannerlImage.pixmap().rect() )
+            # qp.drawPixmap( self.ui.bannerlImage.geometry(), pix, self.ui.bannerlImage.geometry() )
+        if self.font is not None :
+            qp.setFont( self.font )
+        if self.color is not None :
+            qp.setPen(self.color)
+        
+        qp.drawText( self.ui.bannerLabel.geometry(), QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter,  self.ui.bannerLabel.text());
+        image.save("output.png")
+        qp.end()
+
+
     def closeWidget(self) :
+        self.saveImage()
         self.closeApp.emit()
         self.close()
+
 
 
 def showPreview(  ) :
