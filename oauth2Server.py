68eb96b5a35f003ceebc861d20c859722fdc7dd2
Pratik Anand <pratik.preet@gmail.com>
2016-04-20 19:05:05 +0530

    working vimeo. it is now wil be intergrated into the code

---
+++ oauth2Server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+ 
+# from http.server import BaseHTTPRequestHandler, HTTPServer
+import BaseHTTPServer
+import videoVimeo
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+ 
+  
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        print message
+        print self.path
+        print "self.path %s" %self.path
+        CODE_FROM_URL = self.path.split('=')[1]
+        print "CODE FROM URL %s" %CODE_FROM_URL
+
+        videoVimeo.processResoponse( CODE_FROM_URL )
+        return
+
+
+def run():
+  print 'starting server...'
+ 
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('127.0.0.1', 8081)
+  httpd = BaseHTTPServer.HTTPServer(server_address, testHTTPServer_RequestHandler)
+  # httpd.setCallback( videoVimeo.processResoponse )
+  print 'running server...'
+  httpd.serve_forever()
+ 
+ 
+run()
--- videoVimeo.py
+++ videoVimeo.py
@@ -1,19 +1,28 @@
 import vimeo
+import webbrowser
 
-v = vimeo.VimeoClient(
-    key="3baa464c8b42baf01019bacfef971e9e939a42c0",
-    secret="8vNFwEfVmT+nu6okpSmzkFXuUTvjJPyUTIySqyEPYlbQsr+C2OAPhY+uunbY3RE0P9tFVrwxpKbznbErzpcuue6UMhTdn70bJKQVl4OAgHFGif2pVq/aspdxE/5neJjJ")
+def requestOAuth() :
+  v = vimeo.VimeoClient(
+      key="3baa464c8b42baf01019bacfef971e9e939a42c0",
+      secret="8vNFwEfVmT+nu6okpSmzkFXuUTvjJPyUTIySqyEPYlbQsr+C2OAPhY+uunbY3RE0P9tFVrwxpKbznbErzpcuue6UMhTdn70bJKQVl4OAgHFGif2pVq/aspdxE/5neJjJ")
 
-vimeo_authorization_url = v.auth_url(['public', 'private','upload'], 'https://api.vimeo.com/oauth/authorize','')
+  vimeo_authorization_url = v.auth_url(['public', 'private','upload'], 'http://localhost:8081','')
+  webbrowser.open(vimeo_authorization_url)
 
-try:
-    token, user, scope = v.exchange_code(vimeo_authorization_url, 'https://api.vimeo.com/oauth/access_token')
-    print scope
-except vimeo.auth.GrantFailed:
-    # Handle the failure to get a token from the provided code and redirect.
-    print token
+def processResoponse(CODE_FROM_URL) :
+  """This section completes the authentication for the user."""
+  v = vimeo.VimeoClient(
+          key="3baa464c8b42baf01019bacfef971e9e939a42c0",
+          secret="8vNFwEfVmT+nu6okpSmzkFXuUTvjJPyUTIySqyEPYlbQsr+C2OAPhY+uunbY3RE0P9tFVrwxpKbznbErzpcuue6UMhTdn70bJKQVl4OAgHFGif2pVq/aspdxE/5neJjJ")
+  # You should retrieve the "code" from the URL string Vimeo redirected to.  Here that's named CODE_FROM_URL
+  try:
+      token, user, scope = v.exchange_code(CODE_FROM_URL, 'http://localhost:8081')
+      print token
+      print scope
+  except vimeo.auth.GrantFailed:
+      print "tokening failed"
+      # Handle the failure to get a token from the provided code and redirect.
+  video_uri = v.upload('C:/Users/pratika/Desktop/valve.avi')
+  v.patch(video_uri, data={'name': 'Video title', 'description': '...'})
+  # Store
 
-video_uri = v.upload('C:\Python27\codes\/vide_mani\/new_video_kind175.mp4')
-
-
-#v.upload_picture('/videos/12345', 'C:\Python27\codes\vide_mani\a.jpg', activate=True)
