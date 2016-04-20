168f5e7db6d7eac308e23b79aaacbf2b4d6c8be9
Pratik Anand <pratik.preet@gmail.com>
2016-04-21 00:37:19 +0530

    modified video vimeo so that upload goes through. Seems to work. Need to test more

--- videoVimeo.py
+++ videoVimeo.py
@@ -5,7 +5,8 @@ import oauth2Server
 
 
 caller = None
-
+KEY="3baa464c8b42baf01019bacfef971e9e939a42c0"
+SECRET="8vNFwEfVmT+nu6okpSmzkFXuUTvjJPyUTIySqyEPYlbQsr+C2OAPhY+uunbY3RE0P9tFVrwxpKbznbErzpcuue6UMhTdn70bJKQVl4OAgHFGif2pVq/aspdxE/5neJjJ"
 def requestOAuth() :
   oauth2Server.callback = processResoponse
   
@@ -13,8 +14,8 @@ def requestOAuth() :
   t.start()
 
   v = vimeo.VimeoClient(
-      key="3baa464c8b42baf01019bacfef971e9e939a42c0",
-      secret="8vNFwEfVmT+nu6okpSmzkFXuUTvjJPyUTIySqyEPYlbQsr+C2OAPhY+uunbY3RE0P9tFVrwxpKbznbErzpcuue6UMhTdn70bJKQVl4OAgHFGif2pVq/aspdxE/5neJjJ")
+      key=KEY,
+      secret=SECRET)
 
   vimeo_authorization_url = v.auth_url(['public', 'private','upload'], 'http://localhost:8081','')
   webbrowser.open(vimeo_authorization_url)
@@ -22,8 +23,8 @@ def requestOAuth() :
 def processResoponse(CODE_FROM_URL) :
   """This section completes the authentication for the user."""
   v = vimeo.VimeoClient(
-          key="3baa464c8b42baf01019bacfef971e9e939a42c0",
-          secret="8vNFwEfVmT+nu6okpSmzkFXuUTvjJPyUTIySqyEPYlbQsr+C2OAPhY+uunbY3RE0P9tFVrwxpKbznbErzpcuue6UMhTdn70bJKQVl4OAgHFGif2pVq/aspdxE/5neJjJ")
+          key=KEY,
+          secret=SECRET)
   # You should retrieve the "code" from the URL string Vimeo redirected to.  Here that's named CODE_FROM_URL
   try:
       token, user, scope = v.exchange_code(CODE_FROM_URL, 'http://localhost:8081')
@@ -33,13 +34,16 @@ def processResoponse(CODE_FROM_URL) :
       print "tokening failed"
 
   print caller
-  caller.vimeoObj = v
+  caller.vimeoObj = token
   
   # Store
 
 def upload(vimeoObj, VidLocation, titleVid, description, tag) :
-  video_uri = vimeoObj.upload(VidLocation)
-  vimeoObj.patch(video_uri, data={'name': titleVid, 'description': description })
+    v = vimeo.VimeoClient(token=vimeoObj,
+          key=KEY,
+          secret=SECRET)
+    video_uri = v.upload(VidLocation)
+    v.patch(video_uri, data={'name': titleVid, 'description': description })
 
 
 if __name__ == "__main__":
